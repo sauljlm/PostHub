bb30218bd515bdb4c6aa4304bd22ba5f4551b0de
saul lopez <saulsjlmclash@gmail.com>
2023-11-09 20:08:17 -0600

    agrego estructura simple para proyecto

--- post-hub/src/index.js
+++ post-hub/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Home from "./pages/home";
+import './sass/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const queryClient = new QueryClient();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+    
+      <Routes>
+        <Route exact path="" element={<Home></Home>} />
+      </Routes>
+
+    </QueryClientProvider>
+  </BrowserRouter>
+);
---
+++ post-hub/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <p>Home page</p>
+    </div>
+  );
+};
+
+export default home;
