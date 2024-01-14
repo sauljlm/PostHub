8d8031662effc19378b927bb1f5f50176648872a
saul lopez <saulsjlmclash@gmail.com>
2024-01-13 22:04:16 -0600

    agrego funcionalidad del menu

--- post-hub/src/components/navBar.js
+++ post-hub/src/components/navBar.js
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 const Navbar = () => {
     const [currentUser, setCurrentUser] = useState(false);
-    const [activeLink, setActiveLink] = useState(null);
+    const [activeLink, setActiveLink] = useState(0);
+    const [hamburguerActive, setHamburguerActive] = useState(false);
 
     const pathname = window.location.pathname;
+
     const navigationLogged = [
-        { title: "Inicio", path: "#start" },
-        { title: "Nuevo post", path: "#newPost" },
-        { title: "My perfil", path: "#ourCustomers" }
+        { title: "Inicio", path: "/" },
+        { title: "Nuevo post", path: "/newPost" },
+        { title: "My perfil", path: "/ourCustomers" }
     ];
     const navigationMain = [
-        { title: "Inicio", path: "#start" },
-        { title: "Iniciar Sesion", path: "#logIn" },
-        { title: "Registrarse", path: "#signUp" }
+        { title: "Inicio", path: "/" },
+        { title: "Iniciar Sesion", path: "/logIn" },
+        { title: "Registrarse", path: "/signUp" }
     ];
 
     async function updateLoggedUser() {
@@ -22,32 +24,32 @@ const Navbar = () => {
 
     const handleLinkClick = (idx) => {
         setActiveLink(idx);
-      };
+    };
     
-      const renderMenuItems = (navigationItems) => {
+    const renderMenuItems = (navigationItems) => {
         return navigationItems.map((item, idx) => (
-          <li
-            key={idx}
-            className={`menu__link ${idx === activeLink ? ' menu__link--active' : ''}`}
-          >
-            <a href={item.path} onClick={() => handleLinkClick(idx)}>
-              {item.title}
-            </a>
-          </li>
+            <li
+                key={idx}
+                className={`menu__link ${item.path === pathname ? ' menu__link--active' : ''}`}
+            >
+                <a href={item.path} onClick={() => handleLinkClick(idx)}>
+                {item.title}
+                </a>
+            </li>
         ));
-      };
+    };
 
     return (
         <header className="header">
             <div className="mobile-nav">
-                <div className="hamburguer">
-                    <svg className="hamburguer__btn">
+                <div className="hamburguer" onClick={() => setHamburguerActive(!hamburguerActive)}>
+                    <svg className={`hamburguer__btn ${hamburguerActive ? ' hamburguer__btn--active' : ''}`}>
                         <path className="line line-top" d="M0,9h30"/>
                         <path className="line line-center" d="M0,15h30"/>
                         <path className="line line-bottom" d="M0,21h30"/>
                     </svg>
                 </div>
-                <nav className="menu">
+                <nav className={`menu ${hamburguerActive ? ' menu--show' : ''}`}>
                     <ul className="menu__container">
                     {currentUser ? renderMenuItems(navigationLogged) : renderMenuItems(navigationMain)}
                     </ul>
